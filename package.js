e508f822498a7689901cfd89e80b8fcd47a94f21
Matteo De Micheli <mat.demicheli@gmail.com>
2014-08-27 19:32:01 +0200

    New version 1.0.5 for new packaging system

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'matteodem:easy-search',
   summary: "Easy-to-use search with Blaze Components (+ Elastic Search support)",
-  version: "1.0.4",
+  version: "1.0.5",
   git: "https://github.com/matteodem/meteor-easy-search.git"
 });
 
