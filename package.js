0da50a9188500b86b2f98738b4327c0b4acca06b
Matteo De Micheli <mat.demicheli@gmail.com>
2014-12-10 22:57:05 +0100

    New version 1.3.8 for fixes with observing collections

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'matteodem:easy-search',
   summary: "Easy-to-use search with Blaze Components (+ Elastic Search support)",
-  version: "1.3.7",
+  version: "1.3.8",
   git: "https://github.com/matteodem/meteor-easy-search.git"
 });
 
