ffd34456fb1c9a7586a71414ec3fc704197afa84
Matteo De Micheli <mat.demicheli@gmail.com>
2015-01-10 12:53:16 +0100

    Version 1.4.6 with minor fixes

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'matteodem:easy-search',
   summary: "Easy-to-use search with Blaze Components (+ Elastic Search support)",
-  version: "1.4.5",
+  version: "1.4.6",
   git: "https://github.com/matteodem/meteor-easy-search.git"
 });
 
