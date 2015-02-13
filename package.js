94232e1554fad0f34f6cad9e43100712fbfcb286
Matteo De Micheli <mat.demicheli@gmail.com>
2015-02-13 21:04:43 +0100

    Remove components from initialized array when template is destroyed

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'matteodem:easy-search',
   summary: "Easy-to-use search with Blaze Components (+ Elastic Search support)",
-  version: "1.4.11",
+  version: "1.4.12",
   git: "https://github.com/matteodem/meteor-easy-search.git"
 });
 
