cac7da61a534cb7d7d64ffdc0e8264d683e3a861
Matteo De Micheli <mat.demicheli@gmail.com>
2015-02-22 21:35:03 +0100

    Add missing tests for component API

--- tests/components-tests.js
+++ tests/components-tests.js
@@ -41,3 +41,173 @@ Tinytest.add('EasySearch - Components - clear', function (test) {
   test.equal(instance.get('searchResults'), []);
 });
 
+Tinytest.add('EasySearch - Components - paginate', function (test) {
+  var originalGetIndex = EasySearch.getIndex;
+
+  Session.set('esVariables_fakeIndex_paginationSkip', -1);
+  Session.set('esVariables_fakeIndex_currentControl', -1);
+
+  EasySearch.getIndex = function () {
+    return { reactive: false, limit: 20, name: 'fakeIndex' };
+  };
+
+  EasySearch.getComponentInstance(fakeConf).paginate(1);
+
+  test.equal(EasySearch.getComponentInstance(fakeConf).get('paginationSkip'), 0);
+  test.equal(EasySearch.getComponentInstance(fakeConf).get('currentControl'), 1);
+  
+  EasySearch.getComponentInstance(fakeConf).paginate(2);
+
+  test.equal(EasySearch.getComponentInstance(fakeConf).get('paginationSkip'), 20);
+  test.equal(EasySearch.getComponentInstance(fakeConf).get('currentControl'), 2);
+  
+  EasySearch.getIndex = originalGetIndex;
+});
+
+Tinytest.addAsync('EasySearch - Components - search', function (test, done) {
+  var originalGetIndex = EasySearch.getIndex,
+    originalSearch = EasySearch.search,
+    instance = EasySearch.getComponentInstance(fakeConf);
+
+  EasySearch.getIndex = function () {
+    return { reactive: false };
+  };
+
+  EasySearch.search = function (index, searchValue, cb) {};
+
+  Session.set('esVariables_fakeIndex_searching', true);
+  Session.set('esVariables_fakeIndex_searchingDone', true);
+  Session.set('esVariables_fakeIndex_currentValue', 'notSomething');
+
+  instance.search('something');
+
+  test.equal(instance.get('currentValue'), 'something');
+
+  instance.search('');
+
+  test.equal(instance.get('searching'), false);
+  test.equal(instance.get('searchingDone'), false);
+  
+  setTimeout(function () {
+    EasySearch.getIndex = originalGetIndex;
+    EasySearch.search = originalSearch;
+    
+    done();
+  }, 100);
+});
+
+Tinytest.addAsync('EasySearch - Components - triggerSearch (not reactive)', function (test, done) {
+  var originalGetIndex = EasySearch.getIndex,
+    originalSearch = EasySearch.search,
+    instance = EasySearch.getComponentInstance(fakeConf);
+
+  EasySearch.getIndex = function () {
+    return { reactive: false };
+  };
+
+  EasySearch.search = function (index, searchValue, cb) {
+    var results = [{ _id: 10, value: 100 }, { _id: 2, value: 200 }];
+
+    test.equal(index, 'fakeIndex');
+    test.equal(searchValue, 'something');
+
+    cb(null, {
+      total: results.length,
+      results: results
+    });
+
+    test.equal(instance.get('searching'), false);
+    test.equal(instance.get('total'), results.length);
+    test.equal(instance.get('searchingDone'), true);
+    test.equal(instance.get('searchResults'), results);
+
+    EasySearch.getIndex = originalGetIndex;
+    EasySearch.search = originalSearch;
+
+    done();
+  };
+
+  Session.set('esVariables_fakeIndex_currentValue', 'something');
+  instance.triggerSearch();
+});
+
+Tinytest.addAsync('EasySearch - Components - triggerSearch (reactive)', function (test, done) {
+  var originalGetIndex = EasySearch.getIndex,
+    originalSubscribe = Meteor.subscribe,
+    instance = EasySearch.getComponentInstance(fakeConf);
+  
+  EasySearch.getIndex = function () {
+    return { name: 'fakeIndex', reactive: true, use: 'mongo-db' };
+  };
+
+  Meteor.subscribe = function (subscriptionName, config, cb) {
+    test.equal(subscriptionName, 'fakeIndex/easySearch');
+
+    test.instanceOf(config, Object);
+    
+    cb();
+
+    test.equal(instance.get('searching'), false);
+    test.equal(instance.get('searchingDone'), true);
+
+    EasySearch.getIndex = originalGetIndex;
+    Meteor.subscribe = originalSubscribe;
+
+    done();
+  };
+
+  instance.triggerSearch();
+});
+
+// Autosuggest
+
+Tinytest.add('EasySearch - Components - resetAutosuggest', function (test) {
+  var instance = EasySearch.getComponentInstance(fakeConf),
+    called = false;
+
+  Session.set('esVariables_fakeIndex_autosuggestSelected', [{ id: 'testId', value: 'testValue' }]);
+
+  instance.clear = function () {
+    called = true;
+  };
+
+  instance.resetAutosuggest({
+    val: function (emptyString) {
+      test.equal(emptyString, '');
+    }
+  });
+
+  test.equal(called, true);
+  test.equal(instance.get('autosuggestSelected'), []);
+});
+
+Tinytest.add('EasySearch - Components - manageAutosuggestValues', function (test) {
+  var instance = EasySearch.getComponentInstance(fakeConf),
+    beforeValues = [{ id: 'testId', value: 'testValue'}],
+    afterValues = [{ id: 'testId2', value: 'testValue2'}],
+    called = false;
+
+  Session.set('esVariables_fakeIndex_autosuggestSelected', beforeValues);
+
+  test.throws(function () {
+    instance.manageAutosuggestValues()
+  });
+
+  test.equal(instance.get('autosuggestSelected'), beforeValues);
+
+  test.throws(function () {
+    instance.manageAutosuggestValues(function (current) {
+      return { invalid: 'format' };
+    });
+  });
+
+  test.equal(instance.get('autosuggestSelected'), beforeValues);
+  
+  instance.manageAutosuggestValues(function (current) {
+    called = true;
+    return afterValues;
+  });
+
+  test.equal(called, true);
+  test.equal(instance.get('autosuggestSelected'), afterValues);
+});
