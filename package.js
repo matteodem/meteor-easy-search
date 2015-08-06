4aee3786a473e91b831f9cc4c7cb343f2895e987
Matteo De Micheli <mat.demicheli@gmail.com>
2015-08-06 17:43:18 +0200

    Remove files for v2, deprecate package

--- package.js
+++ package.js
@@ -1,51 +1,16 @@
 Package.describe({
   name: 'matteodem:easy-search',
   summary: "Easy-to-use search with Blaze Components (+ Elastic Search support)",
-  version: "1.6.2",
+  version: "2.0.0",
   git: "https://github.com/matteodem/meteor-easy-search.git"
 });
 
-Npm.depends({
-  'elasticsearch': '5.0.0'
-});
-
-Package.on_use(function (api) {
+Package.onUse(function (api) {
   if (api.versionsFrom) {
     api.versionsFrom('METEOR@1.0.4');
   }
 
-  api.use(['underscore', 'livedata', 'mongo-livedata', 'meteor',
-    'meteor-platform', 'less', 'meteorhacks:aggregate@1.2.1'], ['client', 'server']);
+  console.log('matteodem:easy-search is deprecated (use easy:search instead)');
 
-  api.use(['templating', 'ui', 'jquery'], 'client');
-
-  api.add_files(['lib/easy-search-common.js', 'lib/easy-search-convenience.js']);
-
-  api.add_files([
-    'lib/searchers/mongo.js',
-    'lib/easy-search-client.js',
-    'lib/components/easy-search-components.html',
-    'lib/components/easy-search-components.js',
-    'lib/components/easy-search-components.less'
-  ], 'client');
-
-  api.add_files([
-    'lib/searchers/mongo.js',
-    'lib/easy-search-server.js',
-    'lib/searchers/elastic-search.js'
-  ], 'server');
-
-  api.export('EasySearch');
-});
-
-Package.on_test(function (api) {
-  if (api.versionsFrom) {
-    api.use('matteodem:easy-search');
-    api.use('tinytest@1.0.0');
-  } else {
-    api.use(['tinytest', 'easy-search']);
-  }
-
-  api.add_files(['tests/javascript-api-tests.js']);
-  api.add_files(['tests/component-api-tests.js', 'tests/components-tests.js'], 'client');
+  api.use(['easy:search@2.0.0']);
 });
