91adddda62cbc3c20614b46f83b565a54526c9aa
Matteo De Micheli <mat.demicheli@gmail.com>
2015-10-01 20:24:30 +0200

    addProps and removeProps methods for components

---
+++ packages/easysearch:components/tests/unit/component-methods-tests.js
@@ -0,0 +1,46 @@
+Tinytest.add('EasySearch Components - Unit - Component Methods - addProps / removeProps', function (test) {
+  let index = new EasySearch.Index({
+    collection: new Meteor.Collection('setPropsCollection'),
+    engine: new EasySearch.Minimongo(),
+    fields: ['test']
+  });
+
+  index.registerComponent();
+
+  let componentMethods = index.getComponentMethods();
+
+  componentMethods.addProps('customProp', 'customValue');
+
+  test.equal(index.getComponentDict().get('searchOptions'), {
+    props: {
+      customProp: 'customValue'
+    }
+  });
+
+  componentMethods.addProps({
+    what: 'test',
+    aha: 'yeah'
+  });
+
+  test.equal(index.getComponentDict().get('searchOptions'), {
+    props: {
+      what: 'test',
+      aha: 'yeah',
+      customProp: 'customValue'
+    }
+  });
+
+  componentMethods.removeProps('aha', 'customProp');
+
+  test.equal(index.getComponentDict().get('searchOptions'), {
+    props: {
+      what: 'test'
+    }
+  });
+
+  componentMethods.removeProps();
+
+  test.equal(index.getComponentDict().get('searchOptions'), {
+    props: {}
+  });
+});
