e20a208aa748d941612f36c3e176c6838a4a8cd7
Matteo De Micheli <mat.demicheli@gmail.com>
2016-01-03 15:30:29 +0100

    Bump version to 2.0.7 fixes #408

--- packages/easy:search/package.js
+++ packages/easy:search/package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'easy:search',
   summary: "Easy-to-use search with Blaze Components (+ Elastic Search Support)",
-  version: "2.0.6",
+  version: "2.0.7",
   git: "https://github.com/matteodem/meteor-easy-search.git",
   documentation: "../../README.md"
 });
--- packages/easysearch:components/package.js
+++ packages/easysearch:components/package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'easysearch:components',
   summary: "Blaze Components for EasySearch",
-  version: "2.0.6",
+  version: "2.0.7",
   git: "https://github.com/matteodem/meteor-easy-search.git",
   documentation: 'README.md'
 });
