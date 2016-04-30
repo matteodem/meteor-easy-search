623b68d00d4e2a8c574ef5651d551b4f02b988e0
Matteo De Micheli <mat.demicheli@gmail.com>
2016-04-30 11:08:06 +0200

    2.0.10 autosuggest release with unweakened lodash dependency

--- packages/easysearch:autosuggest/package.js
+++ packages/easysearch:autosuggest/package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'easysearch:autosuggest',
   summary: "Selectize Autosuggest Component for EasySearch",
-  version: "2.0.9",
+  version: "2.0.10",
   git: "https://github.com/matteodem/meteor-easy-search.git",
   documentation: 'README.md'
 });
@@ -13,7 +13,7 @@ Package.onUse(function(api) {
   api.use(['check', 'ecmascript', 'templating', 'blaze']);
   api.use(['easysearch:core@2.0.9', 'jeremy:selectize@0.12.1_4']);
 
-  api.use(['erasaur:meteor-lodash@4.0.0'], { weak: true });
+  api.use(['erasaur:meteor-lodash@4.0.0']);
 
   api.addFiles([
     'lib/autosuggest.html',
