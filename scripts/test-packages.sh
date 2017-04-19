b5e7ae88c3226f42417da5577e8fa99adbe48836
Matteo De Micheli <matteodem@protonmail.com>
2017-04-19 15:38:07 +0200

    run both pkg in one command

--- scripts/test-packages.sh
+++ scripts/test-packages.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-spacejam test-packages ./packages/easysearch:core
-spacejam test-packages ./packages/easysearch:components
+cd ./packages && spacejam test-packages ./easysearch:core ./easysearch:components
