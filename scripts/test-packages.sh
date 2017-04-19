d554d79738576c3d5c7398638ce207ccee7512d8
Matteo De Micheli <matteodem@protonmail.com>
2017-04-19 14:04:29 +0200

    update travis yml

--- scripts/test-packages.sh
+++ scripts/test-packages.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-cd packages/easysearch:core && spacejam test-packages ./ && cd ../../
-cd packages/easysearch:components && spacejam test-packages ./ && cd ../../
-cd packages/easysearch:elasticsearch && spacejam test-packages ./ && cd ../../
+spacejam test-packages ./packages/easysearch:core
+spacejam test-packages ./packages/easysearch:components
+spacejam test-packages ./packages/easysearch:elasticsearch
