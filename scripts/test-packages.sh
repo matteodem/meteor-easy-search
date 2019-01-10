66d39593b71e5383c3f52725fe57e5a02d1f3a7e
Todd Hiles <2toddhiles@gmail.com>
2019-01-10 11:12:55 -0500

    Fix integration scripts to reference Windows friendly directories.

--- scripts/test-packages.sh
+++ scripts/test-packages.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-cd ./packages && spacejam test-packages ./easysearch:core ./easysearch:components
+cd ./packages && spacejam test-packages ./easysearch_core ./easysearch_components
